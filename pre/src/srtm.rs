ce64cae104412b144a92b1c9480fa61d007489f2
Simon Matejetz <simon.mat@t-online.de>
2020-01-04 12:39:43 +0100

    unzipping response directly, saving as .hgt file works

--- pre/src/srtm.rs
+++ pre/src/srtm.rs
@@ -1,5 +1,6 @@
 extern crate reqwest;
 extern crate zip;
+extern crate tempfile;
 
 use std::collections::HashMap;
 use std::env::join_paths;
@@ -7,18 +8,15 @@ use std::fs::File;
 use std::ops::Deref;
 use std::path::{Path, PathBuf};
 use self::reqwest::Error;
-use std::io::{Read, copy};
+use std::io::{Read, copy, Bytes, Write};
 use self::zip::read::ZipFile;
-
-const SRTM_FOLDER: &str = "strm_files";
-const SRTM_EXTENSION: &str = ".hgt";
 
 struct SRTM {
     _files: HashMap<String, GeoElevationFile>,
 }
 
 struct GeoElevationFile {
-    path: PathBuf,
+    path: String,
     latitude: f64,
     longitude: f64,
 }
@@ -32,67 +30,52 @@ impl SRTM {
 
     fn get_elevation(&mut self, latitude: f64, longitude: f64) -> &GeoElevationFile {
         let geo_file = self.get_file(latitude, longitude);
-        println!("hi elevation");
         return geo_file;
     }
 
     fn get_file(&mut self, latitude: f64, longitude: f64) -> &GeoElevationFile {
         let filename = SRTM::get_file_name(latitude, longitude);
-        println!("hi file");
         println!("{}", filename);
         if !self._files.contains_key(filename.as_str()) {
-            let file_found = self.retrieve_or_load_file_data(filename.as_str());
+            let file_found = self.load_file_data(filename.as_str());
             self._files.insert(String::from(filename.as_str()), file_found);
         }
         return self._files.get(filename.as_str()).unwrap();
     }
 
-    fn retrieve_or_load_file_data(&self, file_name: &str) -> GeoElevationFile {
-        let path: PathBuf = SRTM::get_local_path(file_name);
-        if !path.exists() {
-            SRTM::retrieve_srtm_file(file_name);
+    fn load_file_data(&self, file_name: &str) -> GeoElevationFile {
+        let path: String = SRTM::get_relative_strm_file_path(file_name);
+        if Path::new(&path).exists() {
+            SRTM::download_srtm_file(file_name);
         }
         return GeoElevationFile::new(path);
     }
 
-    fn get_local_path(file_name: &str) -> PathBuf {
-        let mut path = PathBuf::from(SRTM_FOLDER);
-        path.push(file_name);
-        path.set_extension(SRTM_EXTENSION);
-        return path;
+    fn get_relative_strm_file_path(file_name: &str) -> String {
+        let path = format!("../strm_files/{}.hgt", file_name);
+        path
     }
 
-    fn retrieve_srtm_file(file_name: &str) -> Result<(), reqwest::Error> {
+    fn download_srtm_file(file_name: &str) -> Result<(), reqwest::Error> {
+        // download zip file into response reader
         let url = format!("https://dds.cr.usgs.gov/srtm/version2_1/SRTM3/Eurasia/{}.hgt.zip", file_name);
+        println!("downloading srtm file from: {}", url);
         let mut response = reqwest::blocking::get(url.as_str())?;
-        println!("{}", response.status());
-        // let zipped_bytes = response.bytes();
-        // let unzipped_bytes = SRTM::unzip_bytes(zipped_bytes);
-        // println!("{:?}", unzipped_bytes);
-        let mut file_location = String::from("../strm_files/");
-        file_location.push_str(file_name);
-        file_location.push_str(".hgt.zip");
+
+        // unzip file
+        let mut unzipped_file = zip::read::read_zipfile_from_stream(&mut response).unwrap().unwrap();
+        let mut file_buf: Vec<u8> = Vec::new();
+        unzipped_file.read_to_end(&mut file_buf);
+
+        // save unzipped file
+        let mut file_location = SRTM::get_relative_strm_file_path(file_name);
         let mut dest = File::create(file_location).unwrap();
-
-
-        copy(&mut response, &mut dest);
+        dest.write_all(&file_buf);
+        println!("saved srtm file to: {}", file_location);
 
         Ok(())
     }
 
-    fn unzip_bytes(zipped_bytes: &[u8]) -> Result<Vec<u8>, zip::result::ZipError> {
-        let mut reader = std::io::Cursor::new(zipped_bytes);
-        let mut zip = zip::ZipArchive::new(reader)?;
-        let mut file = zip.by_index(0).unwrap();
-        let mut unzipped_bytes: Vec<u8> = Vec::<u8>::new();
-        for byte in file.bytes() {
-            match byte {
-                Ok(byte) => {unzipped_bytes.push(byte)},
-                Err(err) => {},
-            }
-        }
-        return Ok(unzipped_bytes);
-    }
 
     fn get_file_name(latitude: f64, longitude: f64) -> String {
         let north_south;
@@ -114,7 +97,7 @@ impl SRTM {
 
 impl GeoElevationFile {
     pub fn new(
-        path: PathBuf
+        path: String
     ) -> Self {
         GeoElevationFile {
             path,
@@ -145,7 +128,7 @@ fn test_srtm() {
 fn test_retrieve_srtm() {
     let name = SRTM::get_file_name(47.678926, 7.639213);
     assert_eq!(name, "N47E007");
-    let result = SRTM::retrieve_srtm_file(name.as_str()).unwrap();
+    let result = SRTM::download_srtm_file(name.as_str()).unwrap();
     println!("{:?}", result);
     assert_eq!(result,());
     assert_eq!(1, 0);
